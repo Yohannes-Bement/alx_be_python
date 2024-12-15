df5a9e87e0c52ea4450479a129be70afad354b81
Yohannes-Bement <johnjesusyohanesbement@gmail.com>
2024-12-15 02:37:41 -0800

    finance_calculator.py

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,11 @@
+# future_age_calculator.py
+
+# Prompting the user for their current age
+current_age = int(input("How old are you? "))  # User input is assumed to be a valid integer
+
+# Calculating the age in 2050
+age_in_2050 = current_age + 27
+
+# Printing the result
+print(f"In 2050, you will be {age_in_2050} years old.")
+
